bf7c2aa20620735289b7d86c4eea11f0f6d4d120
huyquang306 <nguyenhuyquang306@gmail.com>
2023-01-02 10:16:42 +0700

    Feat: config routes, exception handler

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'domain.main_domain' => \App\Http\Middleware\MainDomain::class,
+        'domain.order_domain' => \App\Http\Middleware\OrderDomain::class,
+        'domain.admin_domain' => \App\Http\Middleware\AdminDomain::class,
     ];
 }
