fb676025ee6338ffdd69fce86773b81274e86e85
huyquang306 <nguyenhuyquang306@gmail.com>
2023-01-25 11:05:49 +0700

    Feat: create base view

---
+++ resources/views/customer-order.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <!-- Google Font -->
+    <link href=“https://fonts.googleapis.com/css2?family=Open+Sans&display=swap” rel=“stylesheet”>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <meta name="theme-color" content="#000000" />
+    <title>Customer|Order</title>
+</head>
+<body >
+    <noscript>JavaScript</noscript>
+    <div id="appRoot"></div>
+    <!-- script -->
+    <script src={{ mix("/assets/js/customer-order/app.js") }}></script>
+</body>
+</html>
